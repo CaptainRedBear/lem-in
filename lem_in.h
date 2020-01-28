d740736b2755d97420cd909bc406e9230bddf9c5
hbarnardWTC <wingarkorin@gmail.com>
2020-01-28 12:29:37 +0200

    Whoops

---
+++ lem_in.h
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lem_in.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbarnard <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/28 10:32:52 by hbarnard          #+#    #+#             */
+/*   Updated: 2020/01/28 10:32:53 by hbarnard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LEM_IN_H
+# define LEM_IN_H
+
+# include <stdio.h>
+# include "libft.h"
+
+typedef struct		s_path
+{
+	char			*room_name;
+	struct s_path	*next_room;
+}					t_path;
+
+typedef struct		s_links
+{
+	char			*link;
+	struct s_room	*room;
+	struct s_links	*next;
+}					t_links;
+
+typedef struct		s_room
+{
+	char			*name;
+	t_co_ord		pos;
+	t_links			*links;
+	int				type;
+	int				ant_count;
+	int				x_path;
+	int				dist;
+	int				rcount;
+	struct s_room	*next;
+}					t_room;
+
+
+typedef struct		s_ant
+{
+	int				ant_num;
+	char			*curr_room;
+	t_path			*path;
+}					t_ant;
+
+#endif
